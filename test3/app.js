2f146537b5c04cb19e7490450e4fc802160c7f20
xsz <helxsz@gmail.com>
2015-01-16 17:30:04 +0000

    test
    test

--- test3/app.js
+++ test3/app.js
@@ -1,30 +1,13 @@
-var express = require('express'),
-    app = express(),
-    redis = require('redis'),
-    RedisStore = require('connect-redis')(express),
-    server = require('http').createServer(app);
+var express = require('express');
 
-app.configure(function() {
-  app.use(express.cookieParser('keyboard-cat'));
-  app.use(express.session({
-        store: new RedisStore({
-            host: process.env.REDIS_HOST || 'localhost',
-            port: process.env.REDIS_PORT || 6379,
-            db: process.env.REDIS_DB || 0
-        }),
-        cookie: {
-            expires: false,
-            maxAge: 30 * 24 * 60 * 60 * 1000
-        }
-    }));
+// Constants
+var PORT = 3000;
+
+// App
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
 });
 
-app.get('/', function(req, res) {
-  res.json({
-    status: "ok"
-  });
-});
-
-var port = process.env.HTTP_PORT || 3000;
-server.listen(port);
-console.log('Listening on port ' + port);
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
